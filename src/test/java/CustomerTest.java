44571f1621150fdcc4fe8c067e89151b1bb0553b
alexandergm1 <alexander.1gmac@gmail.com>
2022-02-15 16:05:07 +0000

    Customer has money test passed

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,33 @@
+import cars.Car;
+import components.Brakes;
+import components.Engine;
+import components.Tyre;
+import customer.Customer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTest {
+
+    Customer customer;
+    Car car;
+    Engine engine;
+    Tyre tyre;
+    Brakes brakes;
+
+    @Before
+    public void before(){
+        customer = new Customer(50000.00);
+        engine = new Engine(85, "Petrol", 1500);
+        tyre = new Tyre("Sports", 40);
+        brakes = new Brakes(15);
+        car = new Car(25000.00, "Purple", 4, engine, brakes, tyre);    }
+
+
+    @Test
+    public void hasMoney(){
+        assertEquals(50000.00, customer.getMoney(), 0.0);
+    }
+
+}
