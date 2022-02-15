88fb9fdb0ad61fe41c5a32d881f1ac5c683e0070
camsh69 <camsh69@gmail.com>
2022-02-15 14:27:55 +0000

    first passing tyre test

--- src/main/java/components/Tyre.java
+++ src/main/java/components/Tyre.java
@@ -1,4 +1,20 @@
 package components;
 
 public class Tyre {
+
+    private String tyreType;
+    private int radius;
+
+    public Tyre(String tyreType, int radius) {
+        this.tyreType = tyreType;
+        this.radius = radius;
+    }
+
+    public String getTyreType() {
+        return tyreType;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
 }
--- src/test/java/TyreTest.java
+++ src/test/java/TyreTest.java
@@ -1,2 +1,20 @@
+import components.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TyreTest {
+
+    Tyre tyre;
+
+    @Before
+    public void before() {
+        tyre = new Tyre("Sports", 40);
+    }
+
+    @Test
+    public void hasTyreType() {
+        assertEquals("Sports", tyre.getTyreType());
+    }
 }
