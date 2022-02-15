269b9480cd75960a4d2c00c62c4df458280d0736
camsh69 <camsh69@gmail.com>
2022-02-15 15:30:02 +0000

    adding passing Hybrid car test

--- src/main/java/cars/HybridCar.java
+++ src/main/java/cars/HybridCar.java
@@ -1,12 +1,18 @@
 package cars;
 
+import behaviours.IEngineOutput;
 import components.Brakes;
 import components.Engine;
 import components.Tyre;
 
-public class HybridCar extends Vehicle {
+public class HybridCar extends Vehicle implements IEngineOutput {
 
     public HybridCar(double price, String colour, int numberOfDoors, Engine engine, Brakes brakes, Tyre tyre) {
         super(price, colour, numberOfDoors, engine, brakes, tyre);
+    }
+
+    @Override
+    public String getEngineOutput() {
+        return "vrmpurrrr!";
     }
 }
--- src/test/java/HybridCarTest.java
+++ src/test/java/HybridCarTest.java
@@ -1,2 +1,31 @@
+import cars.ElectricCar;
+import cars.HybridCar;
+import components.Brakes;
+import components.Engine;
+import components.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HybridCarTest {
+
+    HybridCar hybridCar;
+    Engine engine;
+    Tyre tyre;
+    Brakes brakes;
+
+    @Before
+    public void before() {
+        engine = new Engine(85, "Electric", 1500);
+        tyre = new Tyre("Sports", 40);
+        brakes = new Brakes(15);
+        hybridCar = new HybridCar(25000.00, "Cobalt", 4, engine, brakes, tyre);
+    }
+
+    @Test
+    public void hasEngineOutput() {
+        assertEquals("vrmpurrrr!", hybridCar.getEngineOutput());
+    }
 }
+
