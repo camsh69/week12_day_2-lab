90af9f11bbea4cd948c06807781bb7ce3ae4e31b
alexandergm1 <alexander.1gmac@gmail.com>
2022-02-15 16:01:25 +0000

    Customer class created and properties assigned

---
+++ src/main/java/customer/Customer.java
@@ -0,0 +1,23 @@
+package customer;
+
+import cars.Vehicle;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private double money;
+    private ArrayList<Vehicle> ownedVehicles;
+
+    public Customer(double money) {
+        this.money = money;
+        this.ownedVehicles = new ArrayList<>();
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public ArrayList<Vehicle> getOwnedVehicles() {
+        return ownedVehicles;
+    }
+}
