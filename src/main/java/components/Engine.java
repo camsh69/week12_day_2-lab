95bfa63f76fbaff02e4c52e37d5ab1417f3a257b
camsh69 <camsh69@gmail.com>
2022-02-15 14:44:00 +0000

    first Engine passing test

--- src/main/java/components/Engine.java
+++ src/main/java/components/Engine.java
@@ -1,4 +1,26 @@
 package components;
 
 public class Engine {
+
+    private int horsePower;
+    private String fuelType;
+    private int engineSize;
+
+    public Engine(int horsePower, String fuelType, int engineSize) {
+        this.horsePower = horsePower;
+        this.fuelType = fuelType;
+        this.engineSize = engineSize;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public int getEngineSize() {
+        return engineSize;
+    }
 }
--- src/test/java/EngineTest.java
+++ src/test/java/EngineTest.java
@@ -1,2 +1,20 @@
+import components.Engine;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EngineTest {
+
+    Engine engine;
+
+    @Before
+    public void before() {
+        engine = new Engine(85, "Petrol", 1500);
+    }
+
+    @Test
+    public void hasHorsePower() {
+        assertEquals(85, engine.getHorsePower());
+    }
 }
