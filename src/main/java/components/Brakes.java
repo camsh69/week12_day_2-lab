0b16aa43a37f865f25ad60ddbdfaf219dbeebad4
camsh69 <camsh69@gmail.com>
2022-02-15 14:33:25 +0000

    adding passing Brake test

--- src/main/java/components/Brakes.java
+++ src/main/java/components/Brakes.java
@@ -1,4 +1,14 @@
 package components;
 
 public class Brakes {
+
+    private int padSize;
+
+    public Brakes(int padSize) {
+        this.padSize = padSize;
+    }
+
+    public int getPadSize() {
+        return padSize;
+    }
 }
--- src/test/java/BrakesTest.java
+++ src/test/java/BrakesTest.java
@@ -1,2 +1,20 @@
+import components.Brakes;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BrakesTest {
+
+    Brakes brakes;
+
+    @Before
+    public void before() {
+        brakes = new Brakes(15);
+    }
+
+    @Test
+    public void hasPadSize() {
+        assertEquals(15, brakes.getPadSize());
+    }
 }
