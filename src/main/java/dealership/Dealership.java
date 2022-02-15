5a5a6bafd4060e81851f1e07e0a2099e07d1ebc0
alexandergm1 <alexander.1gmac@gmail.com>
2022-02-15 16:15:54 +0000

    created getters and constructor for dealership

--- src/main/java/dealership/Dealership.java
+++ src/main/java/dealership/Dealership.java
@@ -1,4 +1,25 @@
 package dealership;
 
+import cars.Vehicle;
+
+import java.util.ArrayList;
+
 public class Dealership {
+    private ArrayList<Vehicle> newCars;
+    private double till;
+
+    public Dealership(double till) {
+        this.newCars = new ArrayList<>();
+        this.till = till;
+    }
+
+    public ArrayList<Vehicle> getNewCars() {
+        return newCars;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void add_money(double money)
 }
