f16c4294f8bfaab69f87cee95cfab1258da57539
camsh69 <camsh69@gmail.com>
2022-02-15 15:20:11 +0000

    sixth passing Car test

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -47,4 +47,9 @@ public class CarTest {
     public void hasBrakes() {
         assertNotNull(car.getBrakes());
     }
+
+    @Test
+    public void hasTyres() {
+        assertNotNull(car.getTyre());
+    }
 }
